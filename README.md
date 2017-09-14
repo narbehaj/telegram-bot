424c44e7b379422e0e393abfa4583952ceb8aa1c
Narbeh <narbeh@protonmail.com>
2017-09-14 11:36:22 +0430

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Simple Telegram Echo Bot
+
+This is a simple Telegram bot script in Python which uses [Telepot](https://github.com/nickoala/telepot) framework. This script only returns back the sent message.
+
+### Requirements
+
+`pip install telepot`
---
+++ echo.py
@@ -0,0 +1,19 @@
+import telepot
+
+telegram_token = ''
+
+
+def handle(msg):
+    content_type, chat_type, chat_id = telepot.glance(msg)
+    user = bot.getUpdates(allowed_updates='message')
+    message = user[0]['message']['text']
+    
+    bot.sendMessage(chat_id, text=message)
+
+
+bot = telepot.Bot(telegram_token)
+bot.message_loop(handle)
+
+# Keep the program running
+while 1:
+    time.sleep(10)
